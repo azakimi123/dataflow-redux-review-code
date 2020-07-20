1c2999a66f8f1ba651a7705cc2dff3a521869f20
azakimi123 <aaron.zakimi@gmail.com>
2020-07-20 11:29:30 -0600

    redux, props, reducers

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Grandchild from './Grandchild';
 
 const User = props => {
+    let [newName, setNewName] = useState('');
+    console.log(props)
+
+    const nameChange = () => {
+        props.nameFn(newName);
+        setNewName('')
+    }
     return (
         <div>
             <p>{props.username}</p>
+            <Grandchild hometown={props.hometown}/>
+            <input value={newName} onChange={e => setNewName(e.target.value)}/>
+            <button onClick={nameChange}>Change Name</button>
         </div>
     )
 }
